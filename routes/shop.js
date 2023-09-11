51aebfdc0258ea97edca0b8cbd85e62ba87b0173
Dipeshxman11 <dipeshramane9378>
2023-09-12 00:05:59 +0530

    Extra Features Of Cart System

--- routes/shop.js
+++ routes/shop.js
@@ -16,7 +16,7 @@ router.get("/cart", shopController.getCart);
 
 router.get("/cart", shopController.getCart);
 
-// router.post("/cart-delete-item", shopController.postCartDeleteProduct);
+router.post("/cart-delete-item", shopController.postCartDeleteProduct);
 
 // router.post("/create-order", shopController.postOrder);
 
